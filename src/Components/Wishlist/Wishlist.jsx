f06a36c5bb470eb2906a6732bab2e305cd44652e
Fatimei-Zehra <eliyevaf33@gmail.com>
2024-05-13 14:23:52 +0400

    Finish Header

---
+++ src/Components/Wishlist/Wishlist.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart } from '@fortawesome/free-regular-svg-icons';
+import wishlistStyle from "../Wishlist/Wishlist.module.css"
+import MediaStyle from "../GlobalCss/Media/media.css"
+
+function Wishlist() {
+  return (
+    <div>
+        <FontAwesomeIcon icon={faHeart}  className={wishlistStyle.wishlistIcon} id='wishlist' />
+    </div>
+  )
+}
+
+export default Wishlist
