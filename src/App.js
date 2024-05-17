ea7cc3b6cd9695a04eb383df78a302c870db9999
RevanQasimov23 <you@example.com>
2024-05-17 22:12:10 +0400

    Login page 50% finished

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 
-import Header from './Components/Header/Header';
-import Main from './Components/Main/Main';
+// import Header from './Components/Header/Header';
+// import Main from './Components/Main/Main';
 import Login from "../src/Components/Login/Login"
 
 
 function App() {
   return (
     <div className="App">
-      <Header/>  
-      <Main/>
+      {/* <Header/>   */}
+      {/* <Main/> */}
       <Login/>
     </div>
   );
