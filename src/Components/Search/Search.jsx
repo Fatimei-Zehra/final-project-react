806bd89e0213d0df7bbbec8cf7ad117dc567ebf7
Fatimei-Zehra <eliyevaf33@gmail.com>
2024-06-24 15:58:33 +0400

    About page Founders responsive ready

--- src/Components/Search/Search.jsx
+++ src/Components/Search/Search.jsx
@@ -5,18 +5,27 @@ import styleSearch from "../Search/Search.module.css"
 import MediaStyle from "../GlobalCss/Media/media.css"
 import { useState } from 'react';
 import Modal from 'react-modal';
+import { RiCloseCircleLine } from "react-icons/ri";
+import SearchStyle from "./Search.module.css"
+
 
 
 function Search() {
   const [isOpen, setIsOpen] = useState(false);
   const [searchTerm, setSearchTerm] = useState('');
+  const [isTransitioning, setIsTransitioning] = useState(false);
 
   const handleOpenModal = () => {
-    setIsOpen(true);
+    // setIsOpen(true);
+    setIsTransitioning(true);
+    setTimeout(() => setIsOpen(true), 300);
   };
 
   const handleCloseModal = () => {
-    setIsOpen(false);
+    // setIsOpen(false);
+    setIsTransitioning(false);
+
+    setTimeout(() => setIsOpen(false), 300);
   };
 
   const handleInputChange = (e) => {
@@ -28,35 +37,39 @@ function Search() {
     console.log(`Searching for: ${searchTerm}`);
 
     handleCloseModal();
-
-    return (
-      <div>
-        <form action="#" className={styleSearch.form} id='form'>
-          <input type="text" className={styleSearch.input} placeholder='What are you looking for?' />
-          <FontAwesomeIcon icon={faSearch} className={styleSearch.searchIcon} onClick={handleOpenModal} />
-        </form>
-
-
-        <Modal
-          isOpen={isOpen}
-          onRequestClose={handleCloseModal}
-          contentLabel="Search Modal"
-        >
-          <h2>Search</h2>
-          <input
-            type="text"
-            placeholder="Enter search term"
-            value={searchTerm}
-            onChange={handleInputChange}
-          />
-          <button onClick={handleSearch}>Search</button>
-          <button onClick={handleCloseModal}>Close</button>
-        </Modal>
-      </div>
-    )
   }
+  return (
+    <div>
+      <form action="#" className={styleSearch.form} id='form'>
+        <input type="text" className={styleSearch.input} placeholder='What are you looking for?' />
+        <FontAwesomeIcon icon={faSearch} className={styleSearch.searchIcon} onClick={handleOpenModal} />
+      </form>
+
+
+      <Modal
+        isOpen={isOpen}
+        onRequestClose={handleCloseModal}
+        contentLabel="Search Modal"  className={SearchStyle.modalItem}>
+          <div className={SearchStyle.modal}>
+        <input
+          type="text"
+          placeholder="Enter to search "
+          value={searchTerm}
+          onChange={handleInputChange}
+        className={SearchStyle.inputSearch} />
+        {/* <button onClick={handleSearch}>Search</button>
+        <button >Close</button> */}
+        <RiCloseCircleLine onClick={handleCloseModal} className={SearchStyle.closeModal} />
+          </div>
+      </Modal>
+    </div>
+  )
 }
 
 
 
+
 export default Search
+
+
+
